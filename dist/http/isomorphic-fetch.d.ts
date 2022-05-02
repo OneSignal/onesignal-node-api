fccd8b3bcee09b9279dadfcc77d873eeee3c5852
ruslankesheshyan <ruslan@onesignal.com>
2022-05-02 13:40:45 -0700

    Removing dist that have been committed accidentally when .gitignore was removed.

--- dist/http/isomorphic-fetch.d.ts
+++
@@ -1,5 +0,0 @@
-import { HttpLibrary, RequestContext, ResponseContext } from './http';
-import { Observable } from '../rxjsStub';
-export declare class IsomorphicFetchHttpLibrary implements HttpLibrary {
-    send(request: RequestContext): Observable<ResponseContext>;
-}
