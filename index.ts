c6cdfef080aebe41235172c35d70e0523b889c98
onesignal-deploy <deploy@onesignal.com>
2026-01-08 12:43:35 -0800

    feat:  export the `ConfigurationParameters` type (#151)
    Co-authored-by: OneSignal <noreply@onesignal.com>

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ export * from "./http/http";
 export * from "./auth/auth";
 export * from "./models/all";
 export { createConfiguration } from "./configuration"
-export { Configuration } from "./configuration"
+export { Configuration, ConfigurationParameters } from "./configuration"
 export * from "./apis/exception";
 export * from "./servers";
 export { RequiredError } from "./apis/baseapi";
